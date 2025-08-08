f4f70c8b14df5c98be517ba16ae1ec334e93e31f
kauabz049 <kauabzanin49@gmail.com>
2025-08-07 21:31:38 -0300

    tttt

--- src/Trabalho.java
+++ src/Trabalho.java
@@ -22,18 +22,31 @@ public class Trabalho {
 
     public static void main(String[] args) {
 
+      //Define os nome dos produtos
       String produto1  = "Batata frita";
       String produto2  = "Salgado";
       String produto3  = "Achocolatado";
 
+      //Define os preços dos produtos
       Double precoProduto1 = 10.00;
       Double precoProduto2 = 12.00;
       Double precoProduto3 = 4.00;
 
-        System.out.println("Cardápio");
-        System.out.println(produto1 + " - R$" + precoProduto1);
-        System.out.println(produto2 + " - R$" + precoProduto2);
-        System.out.println(produto3 + " - R$" + precoProduto3);
+      //Realiza as operações matemáticas
+      Double valorSomado = precoProduto1 + precoProduto3;
+      Double valorDesconto = valorSomado * 0.05;
+      Double valorFinalComDesconto = valorSomado - valorDesconto;
+
+      //Informa para o usuario o preço dos produtos
+      System.out.println("Cardápio");
+      System.out.println(produto1 + " - R$" + precoProduto1);
+      System.out.println(produto2 + " - R$" + precoProduto2);
+      System.out.println(produto3 + " - R$" + precoProduto3);
+      System.out.println("----------------------------------");
+
+      //Informa para o usuario o valor final da sua compra
+      System.out.println("O valor da " + produto1 + " + " + produto3 + " é de: " + valorSomado);
+      System.out.println("Com desconto aplicado do combo de 5%, o valor final da compra é: " + valorFinalComDesconto);
 
     }
 }
